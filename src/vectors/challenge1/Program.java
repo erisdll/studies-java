7e5cd89136f225071cad25686c8c5ca995455b1d
erisdll <72031082+erisdll@users.noreply.github.com>
2024-03-06 11:45:14 -0300

    vectors challenge 1 done

---
+++ src/vectors/challenge1/Program.java
@@ -0,0 +1,40 @@
+package vectors.challenge1;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Program {
+    public static void main(String[] arguments) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hotel Size?");
+        int hotelSize = scanner.nextInt();
+        Renter[] hotel = new Renter[hotelSize];
+
+        System.out.println("How many rents?");
+        int rentsAmount = scanner.nextInt();
+
+        for (int i = 0; i < rentsAmount; i++) {
+            System.out.println("Rent #" + (i + 1));
+            System.out.println("Name: ");
+            String name = scanner.next();
+            System.out.println("Email: ");
+            String email = scanner.next();
+            System.out.println("room: ");
+            int room = scanner.nextInt();
+
+            hotel[room] = new Renter(name, email);
+        }
+
+        System.out.println("Busy Rooms:");
+
+        IntStream.range(0, hotel.length)
+                .filter(i -> hotel[i] != null)
+                .mapToObj(i -> i + ": " + hotel[i].getName() + ", " + hotel[i].getEmail())
+                .forEach(System.out::println);
+
+    }
+}
---
+++ src/vectors/challenge1/Renter.java
@@ -0,0 +1,27 @@
+package vectors.challenge1;
+
+public class Renter {
+    private String name;
+    private String email;
+
+    public Renter (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
